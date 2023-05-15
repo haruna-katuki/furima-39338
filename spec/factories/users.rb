42cd73a8ca3368b94e4cc62bcb0befa9e5963078
haruna-katuki <131534685+haruna-katuki@users.noreply.github.com>
2023-05-15 20:30:02 +0900

    factory botの修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,7 +3,7 @@ FactoryBot.define do
     Faker::Config.locale = :ja
     nickname              {Faker::Name.name}
     email                 {Faker::Internet.free_email}
-    password              {Faker::Internet.password(min_length: 6)}
+    password              {'ao9Y6tfm3'}
     password_confirmation {password}
     family_name           {Faker::Name.last_name}
     first_name            {Faker::Name.first_name}
