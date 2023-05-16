bfc6b456da8884249766130bb1dcfa85bfcc3ed7
haruna-katuki <131534685+haruna-katuki@users.noreply.github.com>
2023-05-16 18:30:57 +0900

    imageのバリデーションとストロングパラメーターの追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,7 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
+  validates :image, presence: true
   validates :item_name, presence: true, length: { maximum: 40 }
   validates :caption, presence: true, length: { maximum: 1000 }
   validates :category_id, numericality: { other_than: 1 }
