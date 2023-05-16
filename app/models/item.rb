fecf621db0154a2df3a3fbd80c4b48c4fae017cf
haruna-katuki <131534685+haruna-katuki@users.noreply.github.com>
2023-05-16 17:54:39 +0900

    stateのActiveHashの適用

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,7 +5,9 @@ class Item < ApplicationRecord
   validates :item_name, presence: true, length: { maximum: 40 }
   validates :caption, presence: true, length: { maximum: 1000 }
   validates :category_id, numericality: { other_than: 1 }
+  validates :state_id, numericality: { other_than: 1 }
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
+  belongs_to :state
 end
