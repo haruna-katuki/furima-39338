eafe07a655db1022b172a7fd859524d467d68842
haruna-katuki <131534685+haruna-katuki@users.noreply.github.com>
2023-05-16 17:03:48 +0900

    captionのバリデーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,5 @@ class Item < ApplicationRecord
   has_one_attached :image
 
   validates :item_name, presence: true, length: { maximum: 40 }
+  validates :caption, presence: true, length: { maximum: 1000 }
 end
