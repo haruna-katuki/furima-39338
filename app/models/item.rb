2f92f3c86e66c03b910128832ee667dc2e26f59c
haruna-katuki <131534685+haruna-katuki@users.noreply.github.com>
2023-05-16 17:22:36 +0900

    categoryのActiveHashの適用

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,4 +4,8 @@ class Item < ApplicationRecord
 
   validates :item_name, presence: true, length: { maximum: 40 }
   validates :caption, presence: true, length: { maximum: 1000 }
+  validates :category_id, numericality: { other_than: 1 }
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
 end
