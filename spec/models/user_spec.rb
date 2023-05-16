62ba411da823f0025492a2edb4cfbac3dab03678
haruna-katuki <131534685+haruna-katuki@users.noreply.github.com>
2023-05-16 14:23:50 +0900

    first_nameが全角でないと登録できないときのテストコード

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -80,6 +80,9 @@ RSpec.describe User, type: :model do
         expect(@user.errors.full_messages).to include("Family name can't be blank")
       end
       it 'first_nameが全角でないと登録できない' do
+        @user.family_name = 'aaa'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Family name is invalid")
       end
       it 'family_name_kanaが空だと登録できない' do
       end
