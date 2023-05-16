c295645887a9b82ef5a46ea5c2623176a659d9ad
haruna-katuki <131534685+haruna-katuki@users.noreply.github.com>
2023-05-16 19:18:03 +0900

    priceのバリデーションの追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,7 +10,7 @@ class Item < ApplicationRecord
   validates :postage_id, numericality: { other_than: 1 }
   validates :prefecture_id, numericality: { other_than: 1 }
   validates :shipping_day_id, numericality: { other_than: 1 }
-  validates :price, presence: true, numericality: { in: 300..9999999 }
+  validates :price, presence: true, numericality: { in: 300..9999999 }, format: { with: /\A[0-9]+\z/ }
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
