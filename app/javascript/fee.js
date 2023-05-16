c97e5e0571e15fe0b78239dbedad21bc6e88bd73
haruna-katuki <131534685+haruna-katuki@users.noreply.github.com>
2023-05-16 21:00:09 +0900

    販売手数料の計算

--- app/javascript/fee.js
+++ app/javascript/fee.js
@@ -2,6 +2,7 @@ window.addEventListener('load', function(){
 
   const inputPrice = document.getElementById("item-price")
   inputPrice.addEventListener("input",function(){
-    console.log("Input action");
+    const addTaxPrice = inputPrice.value - 0.1
+    document.getElementById("add-tax-price").textContent = addTaxPrice;
   });
 })
