571665fab7410077a0e91bff828c4f31140d6454
haruna-katuki <131534685+haruna-katuki@users.noreply.github.com>
2023-05-16 18:11:07 +0900

    prefectureとshipping_dayのActiveHashの適用

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,9 +7,13 @@ class Item < ApplicationRecord
   validates :category_id, numericality: { other_than: 1 }
   validates :state_id, numericality: { other_than: 1 }
   validates :postage_id, numericality: { other_than: 1 }
+  validates :prefecture_id, numericality: { other_than: 1 }
+  validates :shipping_day_id, numericality: { other_than: 1 }
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :state
   belongs_to :postage
+  belongs_to :prefecture
+  belongs_to :shipping_day
 end
