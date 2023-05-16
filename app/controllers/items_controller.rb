06f3787a1a34877f7d511fc5d5daa60c5ebb5b3c
haruna-katuki <131534685+haruna-katuki@users.noreply.github.com>
2023-05-16 18:24:14 +0900

    ストロングパラメーターの記載

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,5 +7,11 @@ class ItemsController < ApplicationController
   end
 
   def create
+    Item.create(item_params)
+  end
+
+  private
+  def item_params
+    params.repuire(:item).permit(:item_name, :caption, :category_id, :state_id, :postage_id, :prefecture_id, :shipping_day_id, :price).merge(user_id: current_user.id)
   end
 end
