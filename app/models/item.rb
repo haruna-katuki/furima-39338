90bff4685c06e0b04b32b7187e52bd0c497cd2f4
haruna-katuki <131534685+haruna-katuki@users.noreply.github.com>
2023-05-17 19:28:48 +0900

    priceのエラー文の文言の修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -11,7 +11,7 @@ class Item < ApplicationRecord
   validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :shipping_day_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :price, presence: true
-  validates :price, numericality: {only_integer: true, message: 'Half-width number'}
+  validates :price, numericality: {only_integer: true, message: "is not half-width number"}
   validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "is out of setting range"}
 
   extend ActiveHash::Associations::ActiveRecordExtensions
