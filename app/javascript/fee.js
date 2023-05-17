62b024cba7c60913274ffaa194b7f6220e149a08
haruna-katuki <131534685+haruna-katuki@users.noreply.github.com>
2023-05-17 14:20:04 +0900

    小数点切り下げ機能の実装

--- app/javascript/fee.js
+++ app/javascript/fee.js
@@ -3,8 +3,8 @@ window.addEventListener('load', function(){
   const price = document.getElementById("item-price")
   price.addEventListener("input",function(){
     const addTaxPrice = price.value * 0.1
-    document.getElementById("add-tax-price").textContent = addTaxPrice;
+    document.getElementById("add-tax-price").textContent = Math.floor(addTaxPrice);
     const profit = price.value - addTaxPrice
-    document.getElementById("profit").textContent = profit;
+    document.getElementById("profit").textContent = Math.floor(profit);
   });
 })
