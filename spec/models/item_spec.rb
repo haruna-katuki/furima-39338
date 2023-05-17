d3412b70cd35c37c31267af6401c9db1242462f2
haruna-katuki <131534685+haruna-katuki@users.noreply.github.com>
2023-05-17 16:34:52 +0900

    mageが存在していないと登録できないときのテストコード

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -14,6 +14,9 @@ RSpec.describe Item, type: :model do
 
     context '新規登録できないとき' do
       it 'imageが存在していないと登録できない' do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
       end
 
       it 'item_nameが存在していないと登録できない' do
