b134ae0d3e17f3ef779fdf97e112e3dbb647425d
haruna-katuki <131534685+haruna-katuki@users.noreply.github.com>
2023-05-17 17:15:07 +0900

    userに紐づいていないと登録できないときのテストコード

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -98,6 +98,9 @@ RSpec.describe Item, type: :model do
       end
 
       it 'userに紐づいていないと登録できない' do
+        @item.user = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("User must exist")
       end
     end
   end
