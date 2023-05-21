df915fb2da42d190d1071f9261fb59b9daf20c59
haruna-katuki <131534685+haruna-katuki@users.noreply.github.com>
2023-05-21 18:32:58 +0900

    トークン化のためのjsファイルとビューファイルの記述

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -2,29 +2,29 @@ const pay = () => {
   const payjp = Payjp(process.env.PAYJP_PUBLIC_KEY);
   const elements = payjp.elements();
   const numberElement = elements.create('cardNumber');
-  const expMonthElement = elements.create('cardExpiry', { 'dateType': 'month' });
-  const expYearElement = elements.create('cardExpiry', { 'dateType': 'year' });
+  const expiryElement = elements.create('cardExpiry');
   const cvcElement = elements.create('cardCvc');
 
   numberElement.mount('#card-number');
-  expMonthElement.mount('#card-exp-month');
-  expYearElement.mount('#card-exp-year');
+  expiryElement.mount('#card-expiry');
   cvcElement.mount('#card-cvc');
 
-  submit.addEventListener("click", () => {
+  const submit = document.getElementById("button");
+
+  submit.addEventListener("click", (e) => {
+    e.preventDefault();
     payjp.createToken(numberElement).then(function (response) {
       if (response.error) {
       } else {
         const token = response.id;
         const renderDom = document.getElementById("charge-form");
-        const tokenObj = `<input value=${token} name='token' type="hidden">`;
+        const tokenObj = `<input value=${token} type="hidden" name='token'>`;
         renderDom.insertAdjacentHTML("beforeend", tokenObj);
-        debugger;
       }
       numberElement.clear();
-      expMonthElement.clear();
-      expYearElement.clear();
+      expiryElement.clear();
       cvcElement.clear();
+      document.getElementById("charge-form").submit();
     });
   });
 }
