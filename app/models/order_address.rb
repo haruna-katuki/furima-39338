9b106c7634a55ff3dbf268b0528533161f809b78
haruna-katuki <131534685+haruna-katuki@users.noreply.github.com>
2023-05-21 14:06:27 +0900

    Formオブジェクトのバリデーションの記述

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,4 +1,12 @@
 class OrderAddress
   include ActiveModel::Model
   attr_accessor :postal_code, :prefecture_id, :city, :house_number, :building, :telephone_number, :user_id, :item_id
+
+  validates :postal_code, presence: true, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+  validates :prefecture_id, numericality: {other_than: 1, message: "can't be blank"}
+  validates :city, presence: true
+  validates :house_number, presence: true
+  validates :telephone_number, presence: true, format: {with: /\A[0-9]{11}\z/, message: "is invalid."}
+  validates :user_id, presence: true
+  validates :item_id, presence: true
 end
