1daf4749c3d5a66b27d33738e930297711ffba44
haruna-katuki <131534685+haruna-katuki@users.noreply.github.com>
2023-05-22 16:38:26 +0900

    全ての項目が存在すれば購入できるときのテストコードとインスタンスの生成の修正

--- spec/models/order_address_spec.rb
+++ spec/models/order_address_spec.rb
@@ -1,15 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe OrderAddress, type: :model do
-  before do
-    @user = FactoryBot.build(:user)
-    @item = FactoryBot.build(:item)
-    @order_address = FactoryBot.build(:order_address, user_id: @user.id, item_id: @item.id)
-  end
-
   describe '商品購入機能' do
+    before do
+      @user = FactoryBot.create(:user)
+      @item = FactoryBot.create(:item)
+      @order_address = FactoryBot.build(:order_address, user_id: @user.id, item_id: @item.id)
+    end
+  
     context '商品の購入ができる場合' do
       it 'postal_code、prefecture_id、city、house_number、building、telephone_number、tokenが存在すれば購入できる' do
+        expect(@order_address).to be_valid
       end
 
       it 'buildingが存在しなくても購入できる' do
